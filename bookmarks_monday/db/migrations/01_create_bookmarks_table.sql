e004f8e2c375a06b16b544944581b4bb209e503f
Katy Day <day.katy@gmail.com>
2021-01-25 17:11:31 +0000

    updated DB by inserting values

--- bookmarks_monday/db/migrations/01_create_bookmarks_table.sql
+++ bookmarks_monday/db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,5 @@
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
+INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');
+INSERT INTO bookmarks VALUES(4, 'http://google.com');
+INSERT INTO bookmarks VALUES (2, 'http://www.destroyallsoftware.com');
